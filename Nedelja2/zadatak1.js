808473282cbd6e3f1f8077ca0de81b4a3c59c050
MilosRistic1987 <risticmilos1987@gmail.com>
2020-02-03 19:26:19 +0100

    Uredjen zaddatak1

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,8 @@
+var a = 3;
+var b = 5;
+console.log(`Zbir brojeva je: ${a + b}`);
+console.log(`Razlika brojeva je: ${a - b}`);
+console.log(`Proizvod brojeva je: ${a * b}`);
+console.log(`Kolicnik brojeva je: ${a / b}`);
+console.log(`Ostatak pri deljenju je: ${a % b}`);
+console.log(`Stepen brojeva je: ${a ** b}`);
